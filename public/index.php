e20a7544fc161c28a7786a5f65c4f9dd13f68419
Yovann AUDINET <yovann.audinet@orange.fr>
2022-08-28 16:28:54 +0200

    "changed index php"

--- public/index.php
+++ public/index.php
@@ -7,14 +7,3 @@ require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
-
-//Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
